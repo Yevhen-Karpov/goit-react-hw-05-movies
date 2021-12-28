35d0459cd8733540005e8479f5c2c68b2ee1aacf
Yevhen-Karpov <goldblogart@gmail.com>
2021-12-28 13:27:38 +0200

    1

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './component/App/App';
+// import s from '../src/component/App/App.module.css';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>{/* <App className={s.app} /> */}</React.StrictMode>,
+  document.getElementById('root'),
+);
